b0458128fcf21efaf40d2a46b39e645e507e264c
unknown <baudinopierreolivier@gmail.com>
2023-02-16 12:55:02 +0100

    action with voice on player

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ibm-js-api
+
 vocal recognition
+
+#how to:
+Appuyer sur le bouton et donner une instruction (une seule à la fois/chaque click boutton) qui suit:
+-lance (music play)
+-pause (music pause)
+-passe (next music)
+-reviens (prev music)
--- index.js
+++ index.js
@@ -10,6 +10,32 @@ recognition.interimResults = false;
 //on ne souhaite pas avoir les résultats de l'interprétation en cours d'écoute
 recognition.maxAlternatives = 1;
 
+// !-------------------------------------
+// *Audio
+const audio = document.getElementById("playerAudio");
+const audioTrack = [
+  "./list/cinematic-documentary-115669.mp3",
+  "./list/electronic-rock-king-around-here-15045.mp3",
+  "./list/tunkhatunkha-20948.mp3",
+];
+var index = 0;
+function nextAudio() {
+  index++;
+  if (index > audioTrack.length - 1) {
+    index = 0;
+  }
+  audio.src = audioTrack[index];
+}
+function prevAudio() {
+  index--;
+  if (index < 0) {
+    index = audioTrack.length - 1;
+  }
+  audio.src = audioTrack[index];
+}
+
+// !--------------------------------
+
 var lancer = document.querySelector("#go");
 // on sélectionne un élément de la page
 
@@ -26,12 +52,22 @@ var question = [
   "dans quel pays est montpellier",
   "j'ai faim",
   "j'ai soif",
+  "dis-moi",
+  "distance",
+  "bonjour",
+  "quel âge as-tu",
+  "comment s'appelle le dernier dinosaure",
 ];
 var reponse = [
   "dans le département de l'hérault",
   "en France",
   "va manger",
   "va boire de l'eau",
+  "moi",
+  "stance",
+  "wesh",
+  "moins que toi!",
+  "denver",
 ];
 
 // si un événement de reconnaissance fonctionne, on récupère le résultat
@@ -39,10 +75,43 @@ recognition.onresult = function (event) {
   var textReconnu = event.results[0][0].transcript;
   console.log("entendu: " + textReconnu);
 
-  for (key in question) {
+  for (let key in question) {
     //on cherche la similitude dans la base pour afficher le résultat
     if (question[key] == textReconnu.toLowerCase()) {
       console.log(reponse[key]);
+    }
+    switch (textReconnu.toLowerCase()) {
+      case "lance":
+        audio.play();
+        return console.log("lot");
+      case "pause":
+        audio.pause();
+        return console.log("et je coupe le son");
+      case "passe":
+        nextAudio();
+        audio.play();
+        return console.log("le message à ton voisin");
+      case "reviens":
+        prevAudio();
+        audio.play();
+        return console.log("vite");
+      case "baisse":
+        if (audio.volume > 0) {
+          audio.volume -= 0.5;
+          return console.log("son baissé");
+        }
+        audio.volume = 0;
+        return console.log("le son est au min");
+      case "fort":
+        if (audio.volume < 1) {
+          audio.volume += 0.5;
+          return console.log("son monté");
+        }
+        audio.volume = 1;
+        return console.log("le son est au max");
+      default:
+        console.log("vous pouvez répéter la question ?");
+        break;
     }
   }
 };
